8bca439daf115d9533c49c30b2d1a8acf248a2a1
Fernando Tavera <fer8707@gmail.com>
2021-05-27 12:52:40 -0500

    initial commit

---
+++ src/components/Articles/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+    Link
+} from 'react-router-dom'
+
+export default function Articles(props) {
+    return (
+        <div>
+            Esta es mi pagina de artículos
+
+            {
+                props.todosLosArticulos.map(e =>{
+                    return(
+                        <>
+                            <Link to = {`/${e.slug}`}>
+                                <p>{e.title}</p>
+                            </Link>
+                            
+                            <p>{e.date}</p>
+                            <img src={e.imageUrl} />
+                        </>
+                    )
+                })
+            }
+        </div>
+    )
+}
